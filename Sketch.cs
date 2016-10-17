b36cb52fba030443a522d9d5c5f3e03c31eed1d3
jlau925 <jlau925@aucklanduni.ac.nz>
2016-10-17 19:42:40 +1300

    Add files via upload

---
+++ Sketch.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Sketch : MonoBehaviour {
+
+    public GameObject myPrefab;
+	// Use this for initialization
+	void Start () {
+        var myPrefab = (GameObject)Instantiate(myPrefab, new Vector3(x, y, z), Quaternion.identity);
+        newCube.GetComponent<CubeScript>()RotateSpeed = Random.value;
+    }
+
+    // Update is called once per frame
+    void Update () {
+	
+	}
+}
---
+++ Survey.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+//Make sure you get rid of the namespace that is automatically generated here.
+class Survey
+{
+    //List all the columns of your table here in the same format as below.
+    public int id { get; set; }
+    public string Location { get; set; }
+    public string EcologicalValue { get; set; }
+    public string HistoricalSignificance { get; set; }
+
+}
